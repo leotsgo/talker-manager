25c99f17d92a8eb7af3ce15df786daff7bb4e7f9
leodiber <leonardodiber@gmail.com>
2022-01-14 16:28:22 -0300

    configuracoes iniciais do Typescript

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 const bodyParser = require('body-parser');
 
-const app = express();
+const app: express.Application = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get('/', (_request: Request, response: Response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
